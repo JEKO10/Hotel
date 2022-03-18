8f9e00815d227c6471ee6d587f02c98cf9a43a38
Aleksa Bubanja <aleksa3b@gmail.com>
2022-03-18 22:14:21 +0100

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <NavBar></NavBar>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar() {
+  return (
+    <nav>
+      <h1>Laguna Mini Resort</h1>
+      <ul>
+        <li>Home</li>
+        <li>Rooms</li>
+        <li>About us</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
