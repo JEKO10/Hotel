e6835a57d538562dd1322aec9678c6e41efb647f
Aleksa Bubanja <aleksa3b@gmail.com>
2022-03-23 21:17:12 +0100

    scrolling fix

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
 import About from "./pages/About";
 import Error from "./pages/Error";
+import ScrollTop from "./components/ScrollTop";
 
 function App() {
   return (
     <Router>
-      <NavBar></NavBar>
+      <NavBar />
+      <ScrollTop />
       <Switch>
         <Route exact path="/">
           <Home />
