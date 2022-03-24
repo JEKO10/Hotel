afe13e1a64ce926e95fe348efd5c7b4fc04af582
Aleksa Bubanja <aleksa3b@gmail.com>
2022-03-24 21:05:49 +0100

    Rooms page style

--- src/components/AllRooms.jsx
+++ src/components/AllRooms.jsx
@@ -1,13 +1,22 @@
 import data from "../data";
+import { Link } from "react-router-dom";
 
 function AllRooms() {
   return (
-    <section>
+    <section className="rooms">
       {data.map((room) => {
         const { fields } = room;
         return (
-          <div key={room.id}>
-            <img src={room.banner} alt="IMG" />
+          <div key={room.id} className="room">
+            <div className="container">
+              <img src={room.banner} alt="IMG" />
+              <div className="price">
+                <h2>${fields.price}</h2>
+                <h3>per night</h3>
+              </div>
+              <Link to="/">More Info</Link>
+            </div>
+            <p>{fields.name}</p>
           </div>
         );
       })}
